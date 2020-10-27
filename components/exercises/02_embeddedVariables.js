8514db8158a87319e6f8b31f3cbd192211d5c62b
Jiaqi Gu <919659233@qq.com>
2020-10-27 22:48:40 +0000

    Exercise 2 completed

--- components/exercises/02_embeddedVariables.js
+++ components/exercises/02_embeddedVariables.js
@@ -1,10 +1,46 @@
 import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
 const Demo = () => {
+  const head = "HDip Computer Science - Modules table";
+  const modules = [
+    {
+      name:"DevOps",
+      noLectures:"2",
+      noPracticals:"2"
+    },
+    {
+      name:"Enterprise Web Dev",
+      noLectures:"3",
+      noPracticals:"2"
+
+    }
+  ]
+
+  
   return (
-      <h1>TODO</h1>
+    <table className="table table-bordered">
+    <thead>
+    <h1>{head}</h1>
+      <tr>
+        <th>Name</th>
+        <th>No lectures</th>
+        <th>No practicals</th>
+      </tr>
+    </thead>
+    <tbody >
+      <tr>
+        <th>{modules[0].name}</th>
+        <th>{modules[0].noLectures}</th>
+        <th>{modules[0].noPracticals}</th>
+      </tr>
+      <tr>
+        <th>{modules[1].name}</th>
+        <th>{modules[1].noLectures}</th>
+        <th>{modules[1].noPracticals}</th>
+      </tr>
+    </tbody >
+  </table>
   );
 };
 
